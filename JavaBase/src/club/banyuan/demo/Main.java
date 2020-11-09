0c9e83ca89b06f97146087f9ab728beded75d4c4
gwx <guanwenxuan1998@gmail.com>
2020-11-09 19:40:41 +0800

    first Java program

---
+++ JavaBase/src/club/banyuan/demo/Main.java
@@ -0,0 +1,17 @@
+package club.banyuan.demo;
+
+/**
+ * @author gwx
+ * @version 1.0
+ * @date 2020/11/9下午7:18
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(true);
+        System.out.println("ring");
+        System.out.println('a');
+        System.out.println(123.23);
+        System.out.println(123);
+    }
+}
